cbcc6c43c2b5a7422418a30d801cb396158ba7fe
Ravi <ravikiran.anumula@epam.com>
2020-04-06 01:23:41 +0530

    added login functionality

--- TrainingApp/src/navigation/MainNavigator.js
+++ TrainingApp/src/navigation/MainNavigator.js
@@ -5,6 +5,7 @@ import DrawerNavigator from "./DrawerNavigator";
 import Product from "../screens/Product";
 import { Login } from "../screens/Auth/Login";
 import { Register } from "../screens/Auth/Register";
+import AuthScreen from "../screens/Auth/AuthScreen";
 import { MAIN_ROUTES, AUTH_ROUTES } from "../constants/routes";
 
 const Stack = createStackNavigator();
@@ -18,8 +19,9 @@ export default React.forwardRef((props, ref) => {
           headerShown: false
         }}
       >
-        <Stack.Screen name={AUTH_ROUTES.LOGIN} component={Login} />
-        <Stack.Screen name={AUTH_ROUTES.REGISTER} component={Register} />
+        <Stack.Screen name={AUTH_ROUTES.LOGIN} component={AuthScreen} />
+        {/* <Stack.Screen name={AUTH_ROUTES.LOGIN} component={Login} />
+        <Stack.Screen name={AUTH_ROUTES.REGISTER} component={Register} /> */}
         <Stack.Screen
           name={MAIN_ROUTES.DRAWER}
           component={DrawerNavigator}
--- TrainingApp/src/store/actions/auth.js
+++ TrainingApp/src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { LOGIN, SIGNUP, LOGOUT } from './actionTypes';
 import { AUTH_URL } from '../../constants/endPoints';
 
@@ -49,17 +49,13 @@ export const signup = (email, password) => {
 
 export const login = (loginName, password) => {
   return async dispatch => {
-    const response = await fetch(
-      `${AUTH_URL}/login`,
+    let formData = new FormData();
+    formData.append("loginname", loginName);
+    formData.append("password", password);
+    const response = await fetch(`${AUTH_URL}/login`,
       {
         method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        body: JSON.stringify({
-          loginName,
-          password: password
-        })
+        body: formData
       }
     );
 
@@ -68,7 +64,7 @@ export const login = (loginName, password) => {
     }
 
     const resData = await response.json();
-    dispatch({ type: LOGIN, userId: loginName, token: token });
+    dispatch({ type: LOGIN, userId: loginName, token: resData.token });
 
     saveDataToStorage(loginName, resData.token);
   };
